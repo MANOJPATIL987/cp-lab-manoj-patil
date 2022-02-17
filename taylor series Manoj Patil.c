5a98da9e340beff475aa84a383d5f6dfc9e2d7ee
MANOJPATIL987 <98514734+MANOJPATIL987@users.noreply.github.com>
2022-02-17 11:44:26 +0530

    Update taylor series Manoj Patil.c

--- taylor series Manoj Patil.c
+++ taylor series Manoj Patil.c
@@ -1,8 +1,25 @@
 #include<stdio.h>
-#include<conio.h>
 #include<math.h>
 #define pi 3.142
 void main()
 {
     int degree,i;
+    float x,nume,denom,term,sum=0;
+    printf("Enter the value of Degree\n");
+    scanf("%d",&degree);
+    x=degree*(pi/180);
+    nume=x;
+    denom=1;
+    i=2;
+    do
+    {
+        term=nume/denom;
+        nume=-nume*x*x;
+        denom=denom*i*(i+1);
+        sum=sum+term;
+        i=i+2;
+    }
+    while(fabs(term)>0.0000001);
+    printf("Sin(%d)=%f without using built in fuction\n",degree,sum);
+    printf("SIn(%d)=%f using built in function\n",degree,sin(x));
 }
